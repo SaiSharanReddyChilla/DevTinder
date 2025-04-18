f38b85448c5d46c405fc8535fc2fa6146b167c75
Sai Sharan Reddy <sharanreddy0011@gmail.com>
2025-04-18 15:30:27 +0530

    logout + profile api's

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,11 +1,55 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
+const {
+  validateProfileUpdatePayload,
+  validateProfilePassword,
+} = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
 const profileRouter = express.Router();
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   const { user } = req;
   res.send(user);
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    const { allowed, message } = validateProfileUpdatePayload(req);
+
+    if (!allowed) {
+      throw new Error(message);
+    }
+    const { user } = req;
+    Object.keys(req.body).forEach((key) => (user[key] = req.body[key]));
+    await user.save();
+    res.json({
+      message: `${user?.firstName}, profile has been updated succesfully!`,
+      data: user,
+    });
+  } catch (err) {
+    res.status(400).send("Error: " + err?.message);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { valid, message } = await validateProfilePassword(req);
+    if (!valid) {
+      throw new Error(message);
+    }
+    const { newPassword } = req.body;
+    const passwordHash = await bcrypt.hash(newPassword, 10);
+    const { user } = req;
+    user["password"] = passwordHash;
+    await user.save();
+    res.json({
+      message: `${user?.firstName}, profile password updated successfully!`,
+      data: user,
+    });
+  } catch (err) {
+    res.status(400).send("Error: " + err?.message);
+  }
 });
 
 module.exports = {
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,4 +1,5 @@
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const validateSignUpPayload = (req) => {
   try {
@@ -19,6 +20,81 @@ const validateSignUpPayload = (req) => {
   }
 };
 
+const validateProfileUpdatePayload = (req) => {
+  try {
+    const ALLOWED_FIELDS = [
+      "firstName",
+      "lastName",
+      "email",
+      "age",
+      "gender",
+      "about",
+      "skills",
+      "photoUrl",
+    ];
+    let isAllowed = Object.keys(req.body).every((key) =>
+      ALLOWED_FIELDS.includes(key)
+    );
+    const { about, skills } = req.body;
+    if (!isAllowed) {
+      isAllowed = {
+        allowed: isAllowed,
+        message: "Invalid Update Request!",
+      };
+    } else if (about && about?.length >= 200) {
+      isAllowed = {
+        allowed: false,
+        message: "About cannot exceed 200 characters!",
+      };
+    } else if (skills && Array.isArray(skills) && skills?.length > 10) {
+      isAllowed = {
+        allowed: false,
+        message: "skills cannot exceed 10!",
+      };
+    } else {
+      isAllowed = {
+        allowed: true,
+        message: "",
+      };
+    }
+    return isAllowed;
+  } catch (err) {
+    throw new Error("Validation Failed!!!");
+  }
+};
+
+const validateProfilePassword = async (req) => {
+  const { password, newPassword } = req.body;
+  if (!password || !newPassword) {
+    return {
+      valid: false,
+      message: "Password cannot be empty!",
+    };
+  }
+  const { user } = req;
+  const isPwdValid = await bcrypt.compare(password, user?.password);
+  if (!isPwdValid) {
+    return {
+      valid: false,
+      message: "Incorrect password!",
+    };
+  } else {
+    if (!validator.isStrongPassword(newPassword)) {
+      return {
+        valid: false,
+        message: "New password is not strong!",
+      };
+    } else {
+      return {
+        valid: true,
+        message: "",
+      };
+    }
+  }
+};
+
 module.exports = {
   validateSignUpPayload,
+  validateProfileUpdatePayload,
+  validateProfilePassword,
 };
